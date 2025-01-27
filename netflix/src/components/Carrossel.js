e30d9d11a35435d178a115cd58b2a02d476fb65e
emilly006 <159432630+emilly006@users.noreply.github.com>
2025-01-27 16:46:20 -0300

    27/01/2025

--- netflix/src/components/Carrossel.js
+++ netflix/src/components/Carrossel.js
@@ -1,1 +1,22 @@
 import './Carrossel.js'
+
+
+import React from "react";
+import { FaNetflix } from "react-icons/fa";
+
+const ImageWithLogo = () => {
+  return (
+    <div className="image-container">
+      <img
+        className="background-image"
+        src="your-image-url.jpg" // Substitua pela URL da sua imagem
+        alt="Background"
+      />
+      <div className="logo">
+        <FaNetflix className="netflix-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default ImageWithLogo;
