968acf600eee28f52a2310ab24b9bcbbec2f3b2a
emilly006 <159432630+emilly006@users.noreply.github.com>
2025-01-28 08:41:55 -0300

    28/01/2025

--- netflix/src/components/Carrossel.js
+++ netflix/src/components/Carrossel.js
@@ -1,22 +1,28 @@
 import './Carrossel.js'
+import flash from '../img/Flahposter.webp'
+import arrow from '../img/Arrow.jpg'
+import gossio from '../img/gossio.jpg'
+import greys from '../img/greys.jpg'
+import pll from '../img/pll.jpg'
 
+function Carrossel(){
+    return(
+      <div className='tudo'>
+        <div className='carrossel'>
+          <div className='texto'>
+            <a> Séries estrangeiras dubladas em português</a>
+          </div>
+          <div className='series'>
+            <img className='fotos' src='Flahposter.webp' />
+            <img className='fotos' src='Arrow.jpg' />
+            <img className='fotos' src='gossio.jpg' />
+            <img className='fotos' src='greys.jpg' />
+            <img className='fotos' src='pll.jpg' />
 
-import React from "react";
-import { FaNetflix } from "react-icons/fa";
-
-const ImageWithLogo = () => {
-  return (
-    <div className="image-container">
-      <img
-        className="background-image"
-        src="your-image-url.jpg" // Substitua pela URL da sua imagem
-        alt="Background"
-      />
-      <div className="logo">
-        <FaNetflix className="netflix-icon" />
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    )
 
-export default ImageWithLogo;
+}
+export default Carrossel
